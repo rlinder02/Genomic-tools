68ffd434127abb620725ffefc5fb2e7542eaa268
Rob Linder <rlinder02@gmail.com>
2022-09-22 10:13:44 -0700

    Modifying seq_classes to accept lists of nucleic acids

--- seq_classes.py
+++ seq_classes.py
@@ -9,11 +9,23 @@ import re
 
 
 class NucleicAcid:
-	"""define a nucliec acid, including unnatural sequences that contain Ts and Us"""
+	"""define a nucliec acid, including unnatural sequences that contain Ts and Us; can be passed in as a single string or a list of strings"""
 
 	_nuc = re.compile('[ATCGUN]+')
 
 	def __init__(self, sequence):
+		if not isinstance(sequence, list) and not isinstance(sequence, str):
+			raise TypeError("Please enter a list of sequence strings or a single sequence string")
+		self.sequence = [seq.upper().replace(" ", "").replace("-", "") for seq in sequence]
+		seq_counter = 0
+		for seq in self.sequence:
+			seq_counter += 1
+			if not NucleicAcid._nuc.fullmatch(self.seq):
+				if isinstance(self.sequence, list):
+					raise ValueError(f"Sequence {seq_counter} is not a valid nucleic acid sequence")
+				else:
+					raise ValueError("Please use a valid nucleic acid sequence")
+
 		self.sequence = sequence.upper().replace(" ", "").replace("-", "")
 		if not NucleicAcid._nuc.fullmatch(self.sequence):
 			raise ValueError("Please use a valid nucleic acid sequence")
@@ -87,25 +99,70 @@ class RNA(NucleicAcid):
 		return ''.join(rts)
 
 
-# class Orf(DNA):
-# 	orfs_found = 0
+class Orf(DNA):
+	"""simplified version that does not deal with introns; effectively the CDS; keeps track of the number of potential ORFs found, which can be reset"""
 
-# 	@classmethod
-# 	def 
-# start = 'ATG'
-# 	stop = ['TAA', 'TAG', 'TGA']
-# 	frame_start = 3
-#class Orf(DNA): 
+	orfs_found = 0
 
-#class Fasta(DNA):
+	_start = 'ATG'
+	_stop = ['TAA', 'TAG', 'TGA']
+
+	@classmethod
+	def display_potential_orfs(cls):
+		return f"There are {cls.orfs_found} potential ORFs"
+
+	def __init__(self, sequence, reset = False):
+		super().__init__(sequence)
+		self.reset = reset
+		if not self.sequence.startswith(Orf._start) or not self.sequence[-3:] in Orf._stop or not len(self.sequence) % 3 == 0:
+			raise ValueError("Please use a valid ORF sequence")
+		if self.reset == True:
+			self.resetter()
+		else:
+			Orf.orfs_found += 1
+
+	def __repr__(self):
+		return f"{self.sequence} is an ORF of length {len(self.sequence)} bases"
+
+	def resetter(self):
+		orfs_found = 0
 
 
+class Fasta(DNA):
+
+	sequences_found = 0
+	
+	@classmethod
+	def display_sequences(cls):
+		return f"There are {cls.sequences_found} sequences in this FASTA file"
+
+	@classmethod
+	def preprocess_fasta(cls, fasta):
+		fasta_dict = {}
+		with open(fasta, 'r') as fa:
+			for line in fa:
+				if line.startswith('>'):
+					key = line.rstrip()
+					fasta_dict[key] = ''
+				else:
+					fasta_dict[key] = fasta_dict[key] + line.rstrip()
+
+		cls(fasta_seqs, fasta_headers)
+
+
+		# keys_values = list(zip(fasta_keys, fasta_values))
+		# fasta_dict.update(keys_values)
+		# return cls(fasta_dict.keys(), fasta_dict.values())
+
+	def __init__(self, sequence, seq_id):
+		super().__init__(sequence)
+		self.seq_id = seq_id
+		Fasta.sequences_found += 1
+
+
+# Adding this in the future
 #class Fastq(DNA):
 
-#class Primer(DNA):
+orf = Orf('ATGUUUTAA')
+print(orf)
 
-#class 
-
-
-# nuc = DNA(sequence = "acgatt")
-# print(nuc.reverse_complement())
